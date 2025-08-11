20b8f98982ec51271f55140d94190ad42d8d2e73
Ayushx27 <phoenixits29@gmail.com>
2025-08-11 18:09:02 +0530

    28774

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Restaurant
+
 def home(request):
-    return render(request,'home/index.html')
+    restaurant = Restaurant.objects.first()
+        return render(request, 'home/index.html', {'restaurant': restaurant})
    
 def about(request):
         return render(request, 'home/about.html')
