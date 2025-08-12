600aa5307812d1a679996275cdfee90e75299ac1
Ayushx27 <phoenixits29@gmail.com>
2025-08-12 21:46:09 +0530

    30414

--- home/views.py
+++ home/views.py
@@ -7,3 +7,12 @@ def home(request):
    
 def about(request):
         return render(request, 'home/about.html')
+def menu(request):
+        menu_items = [
+                {'name': 'Margherita Pizza', 'price': '$12.99', 'description': 'Fresh tomato and mozzarella'},
+                        {'name': 'Chicken Pasta', 'price': '$15.99', 'description': 'Creamy alfredo sauce'},
+                                {'name': 'Caesar Salad', 'price': '$8.99', 'description': 'Fresh greens with dressing'},
+                                        {'name': 'Beef Burger', 'price': '$11.99', 'description': 'Juicy beef with fries'},
+                                                {'name': 'Fish & Chips', 'price': '$13.99', 'description': 'Crispy battered fish'},
+                                                    ]
+                                                        return render(request, 'home/menu.html', {'menu_items': menu_items})
