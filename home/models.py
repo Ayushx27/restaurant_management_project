e2fd651c6ecfdda2d953614043002ed16f1cf590
Ayushx27 <phoenixits29@gmail.com>
2025-08-12 22:00:54 +0530

    30417

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
         description = models.TextField()
-            
-                def __str__(self):
-                        return self.name
+            phone_number = models.CharField(max_length=20, default="(555) 123-4567")
+                
+                    def __str__(self):
+                            return self.name
